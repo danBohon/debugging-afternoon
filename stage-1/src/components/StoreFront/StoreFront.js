188066b253fd206ac5d793c3098ccbcdaf063d08
Daniel Bohon <dan@Daniels-MacBook-Pro.local>
2018-10-23 14:26:33 -0700

    debug stage 1

--- stage-1/src/components/StoreFront/StoreFront.js
+++ stage-1/src/components/StoreFront/StoreFront.js
@@ -14,8 +14,10 @@ class StoreFront extends Component {
     componentDidMount() {
         axios.get("https://practiceapi.devmountain.com/products/")
             .then((response) => {
+                console.log(response);
+                
                 this.setState({
-                    products: response
+                    products: response.data
                 })
             })
     }
